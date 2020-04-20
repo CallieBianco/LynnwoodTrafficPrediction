8b9a328aa79ccc4444a92c7e15c1b29354c5b08d
Callie Bianco <36008290+CallieBianco@users.noreply.github.com>
2020-04-20 14:29:50 -0700

    Refined day_sum() and visualize(), added functions
    - Refined day_sum() 
    - Basic implementation of visualize() done
    - Created test(), need to complete and use
    - Created plots() to use for plotting each set of data

--- LynnwoodTrafficPrediction.py
+++ LynnwoodTrafficPrediction.py
@@ -17,6 +17,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime as dt
 
 # import csv files
 t196_19, t200_19, t196_18, t200_18 = None, None, None, None
@@ -32,40 +33,60 @@ for file in csv:
     tdf = pd.DataFrame(data = df)
     tdf.drop(tdf.tail(4).index, inplace = True)
     tdf["Time"] = pd.to_datetime(tdf["Time"], utc=True)
+    tdf.set_index("Time", inplace=True)
     intersections[i] = tdf
     i+=1
 
 (t196_19, t200_19, t196_18, t200_18) = intersections
 
-def visualize(df):
+def visualize(da, dr):
     """
     Provides a variety of data visualizations
     Parameters:
-    df: DataFrame
+    da: numpy array of daily totals
+    dr: Datetime ndarray storing dates to be plotted
     """
-    x = t196_19["Time"]
-    y = t196_19["Int Total"]
-    plt.plot(x,y)
+    fig, ax = plt.subplots()
+    ax.plot(dr, da, '-oc')
+
+    ax.set(xlabel="Date", ylabel='Total Daily Cars',
+       title='Total Daily Cars at 196th and 44th Sept-Nov 2019'
+       )
+    ax.grid()
     plt.show()
 
 #rng = pd.date_range('1/1/2011', periods=100, freq='1Day')
 #print(rng)
 
-def day_sum(df, daily):
+def day_sum(df, dates):
     """
     Calculates the daily totals using the hourly totals
     Parameters:
     df: DataFrame
-
-    Returns:
     dates: Datetime ndarray storing the dates to be analyzed
+
     daily: Numpy array of daily totals
     """
-    dates = pd.date_range(start='9/1/2019', end='11/30/2019')
-    np.zeros((len(dates),1))
+    daily = np.zeros((len(dates),1))
+    # sums up the hourly intersections totals by day to get daily total
+    daily = df.groupby(df.index.date).sum()
     i = 0
-    for d in dates:
-       y = df.loc[df["Time"].dt.date == d, "Int Total"].sum()
-       daily[i] = y
-       i+=1 
-  
+    return daily
+    
+def tests(df):
+    """
+    Tests data frame accuracy in a variety of ways
+    """
+      
+
+def plots():
+    #drange19 = pd.date_range(start='9/2/2019', end='11/30/2019')
+    #drange18 = pd.date_range(start='9/2/2018', end='11/30/2018')
+    #day196_19 = day_sum(t196_19, drange19)
+    #day196_19 = day196_19[1:-1]
+    #day200_19 = day_sum(t200_19, drange19)
+    #day200_19 = day200_19[1:-1]
+    #day196_18 = day_sum(t196_18, drange18)
+    #day200_18 = day_sum(t196_18, drange18)
+    #print(day196_19)
+    #visualize(day196_19, drange19)
