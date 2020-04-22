34c008b196836a94dd272c43231e0dbeb3b5e1e0
Callie Bianco <36008290+CallieBianco@users.noreply.github.com>
2020-04-22 13:34:26 -0700

    Finished up DataInitialization Class
    Informal testing done, ready for formal testing

--- LynnwoodTrafficPrediction.py
+++ LynnwoodTrafficPrediction.py
@@ -9,7 +9,7 @@
 #
 # Additional Documentation
 # Author: Callie Bianco
-# Version: 1.3 - 4/21/2020
+# Version: 1.0 - 4/15/2020
 # Written for Python 3.7.2
 
 # module imports
@@ -17,15 +17,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
+import seaborn as sns
 
 class DataInitialization:
+    """
+    This class deals with all the initial cleaning and parsing of 
+    the traffic data. It also condenses hourly counts to daily,
+    provides statistical analysis of the data, and visualizations
+    """
     def __init__(self):
         pass
 
-    def main(self):
+    def main(self, visual=None):
+        """
+        Main program to be run in the class
+        """
+        # provide inidivual plots
+        (t196_19, t196_18, t200_19, t200_18) = self.read_files()
+        # A
+        if visual == "A":
+            self.visualize(t196_19, "singleplot", 
+                           "Daily Traffic Counts at 196th and 44th - 2019",
+                           'c')  
+            self.visualize(t196_18, "singleplot", 
+                           "Daily Traffic Counts at 196th and 44th - 2018",
+                           'g')  
+            self.visualize(t200_19, "singleplot", 
+                           "Daily Traffic Counts at 200th and 44th - 2019",
+                           'r')   
+            self.visualize(t200_18, "singleplot", 
+                           "Daily Traffic Counts at 200th and 44th - 2018",
+                           'k')   
+        # combined years
+        # B
+        elif visual == "B":
+            self.visualize(t196_19, "multiplot", 
+                           "Daily Traffic Counts at 196th and 44th 2018-2019",
+                           'b', df1=t196_18)
+            self.visualize(t200_19, "multiplot", 
+                           "Daily Traffic Counts at 200th and 44th 2018-2019",
+                           'b', df1=t200_18)
+
+    def read_files(self):
         # import csv files
         t196_19, t200_19, t196_18, t200_18 = None, None, None, None
-        csv = ["196th_2019.csv", "200th_2019.csv", "196th_2018.csv", "200th_2018.csv"]
+        csv = ["196th_2019.csv", "200th_2019.csv", 
+               "196th_2018.csv", "200th_2018.csv"]
         intersections = [t196_19, t200_19, t196_18, t200_18]
         i = 0
 
@@ -35,90 +72,88 @@ class DataInitialization:
             df = pd.read_csv(file, delimiter = ",", 
                              usecols = ["Time", "Int Total"])
             tdf = pd.DataFrame(data = df)
+            # remove the last few rows that break format
             tdf.drop(tdf.tail(4).index, inplace = True)
+            # convert "Time" to datetime
             tdf["Time"] = pd.to_datetime(tdf["Time"], utc=True)
             tdf.set_index("Time", inplace=True)
             intersections[i] = tdf
             i+=1
 
         (t196_19, t200_19, t196_18, t200_18) = intersections
-        #n = pd.date_range(start='9/2/2019', end='11/30/2019')
-        n1 = self.day_sum(t196_19)
-        n2 = self.day_sum(t196_18)
-        self.visualize(n1, "Stacked bar", n2)
-       
-    def visualize(self, df, ptype, df1):
+        # obtain daily counts instead of hourly
+        t196_19 = self.day_sum(t196_19)
+        t196_18 = self.day_sum(t196_18)
+        t200_19 = self.day_sum(t200_19)
+        t200_18 = self.day_sum(t200_18)
+        return (t196_19, t196_18, t200_19, t200_18)
+    def day_sum(self, df):
+        """
+        Calculates the daily totals using the hourly totals
+        Parameters:
+        df: DataFrame
+
+        Returns:
+        daily: Numpy array of daily totals
+        """
+        daily = df.groupby(df.index.date).sum()
+        return daily
+
+    def visualize(self, df, ptype, title, color, df1 = None):
         """
         Provides a variety of data visualizations
         Parameters:
         da: numpy array of daily totals
         dr: Datetime ndarray storing dates to be plotted
         ptype: String with the name of the plot type
+        title: String with title for graph
+        color: Char for color of graph
         """
         z = pd.date_range(start="9/01/0", end="11/30/0")
         zlen = np.arange(len(z))
         compareType = ptype.lower()
-        if compareType == "Plot".lower():
-            # create a line plot using matplotlib
-            fig, ax = plt.subplots()
-            ax.plot(df, 'c')
 
-            ax.set(xlabel="Date", ylabel='Total Daily Cars',
-               title='Total Daily Cars at 196th and 44th Sept-Nov 2019'
-               )
-            ax.grid()
-            plt.show()
-        elif compareType == "Stacked Bar".lower():
-            # creates a histogram using pandas
+        # plotting a single year
+        if compareType == "SinglePlot".lower():
+            # create a line plot using matplotlib and pandas
+           fig, ax = plt.subplots()
+           ax.plot(df, color)
 
-            # We can set the number of bins with the `bins` kwarg
-            #plt.figure()
-            fig, ax = plt.subplots()
-            rects1 = ax.bar(z, df["Int Total"], label='2019')
-            rects2 = ax.bar(z, df1["Int Total"], label='2018')
-            ax.set_xticks(zlen)
-            ax.set_xticklabels(z)
-            plt.show()
+           ax.set(xlabel="Date", ylabel='Total Daily Cars',
+           title=title)
+           ax.grid()
+           plt.show()
+
+        # plotting multiple years
+        elif compareType == "MultiPlot".lower():
+           frames = [df1, df]
+           df2 = pd.concat(frames)
+           df2["Time"] = df2.index
+           df2["Time"] = pd.to_datetime(df2["Time"], utc=True)
+           df2["Day"] = df2["Time"].dt.dayofyear
+           df2["Year"] = df2["Time"].dt.year
+           print(df2)
+           piv = pd.pivot_table(df2, index="Day",
+                                columns="Year", values=["Int Total"])
+           piv.plot()
+           plt.ylabel("Total Cars")
+           plt.xlabel("Day: Sept. - Nov.")
+           plt.title(title)
+           plt.show()
         else:
             print("Invalid plot type given")
-
-
-    def day_sum(self, df):
-        """
-        Calculates the daily totals using the hourly totals
-        Parameters:
-        df: DataFrame
-        dates: Datetime ndarray storing the dates to be analyzed
-
-        daily: Numpy array of daily totals
-        """
-        daily = df.groupby(df.index.date).sum()
-        return daily
     
-    def tests(df):
+    def tests(self):
         """
         Tests data frame accuracy in a variety of ways
-        """
-      
-
-    #def plots():
-        #Individual plots:
-        # Each intersection, each year
-        # Plot on same graph:
-        # 196th and 200th for each year
-        # All yearly data on a shared graph
-        # Histograms:
-        # All yearly data on a shared graph
-        #drange19 = pd.date_range(start='9/2/2019', end='11/30/2019')
-        #drange18 = pd.date_range(start='9/2/2018', end='11/30/2018')
-        #day196_19 = day_sum(t196_19, drange19)
-        #day196_19 = day196_19[1:-1]
-        #day200_19 = day_sum(t200_19, drange19)
-        #day200_19 = day200_19[1:-1]
-        #day196_18 = day_sum(t196_18, drange18)
-        #day200_18 = day_sum(t196_18, drange18)
-        #print(day196_19)
-        #visualize(day196_19, drange19)
-
+        """     
+        # confirm each daily total consists of 24 hourly periods
+        (t196_19, t196_18, t200_19, t200_18) = self.read_files()
+        print(t196_19)
+        
+# running the program so far
 c = DataInitialization()
-c.main()
+#c.main(visual="B")
+
+# tests
+#c.tests()
